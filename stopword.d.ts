c5da699ed2fa94e9ba690529c85447d1f10006af
Dylan Wahbe <153225786+dwahbe@users.noreply.github.com>
2025-08-09 11:06:18 -0600

    Include local typings in tsconfig

--- stopword.d.ts
+++ stopword.d.ts
@@ -1,3 +1,5 @@
 declare module 'stopword' {
   export function removeStopwords<T extends string | string[]>(input: T): T;
+  const sw: { removeStopwords: typeof removeStopwords };
+  export default sw;
 }
